5128c75915c4fcf11d5e056ca2acd94de8b6df57
Asif Ansari <asifsatyabadi007@gmail.com>
2026-03-14 01:54:18 +0530

    Update App Home UI and Layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,245 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+export default function Home() {
 
-export default function HomeScreen() {
+  const [distance, setDistance] = useState('');
+  const [fuel, setFuel] = useState('');
+  const [milage, setMilage] = useState<string | null>(null);
+
+  const calculateMilage = () => {
+
+    const d = parseFloat(distance);
+    const f = parseFloat(fuel);
+
+
+    if (isNaN(d) || isNaN(f) || f === 0) {
+      alert("Please enter valid numbers");
+      return;
+    }
+
+    const result = d / f;
+    setMilage(result.toFixed(2));
+
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View style={styles.container}>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <View style={styles.header}>
+
+        <View style={styles.headerIcon}>
+          <Text style={styles.headerIconText}>⛽</Text>
+        </View>
+
+        <View style={styles.headerText}>
+          <Text style={styles.title}>Mileage Calculator</Text>
+          <Text style={styles.subTitle}>Metric Fuel Tracker</Text>
+        </View>
+
+      </View>
+
+      <View style={styles.header2}>
+        <Text style={styles.title2}>DISTANCE INPUT</Text>
+      </View>
+
+      <View style={styles.content}>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Distance Travelled (km)</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="e.g. 100"
+            placeholderTextColor="#888"
+            keyboardType="numeric"
+            value={distance}
+            onChangeText={setDistance}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Fuel Used (litres)</Text>
+          <TextInput
+            style={styles.input2}
+            placeholder="e.g. 10"
+            placeholderTextColor="#888"
+            keyboardType="numeric"
+            value={fuel}
+            onChangeText={setFuel}
+          />
+
+          <Text style={styles.inputLabel}>Fuel Price (per litre)</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="e.g. 100"
+            placeholderTextColor="#888"
+            keyboardType="numeric"
+            value={fuel}
+            onChangeText={setFuel}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Trip Label</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="e.g. mumbai to pune"
+            placeholderTextColor="#888"
+            keyboardType="default"
+            value={distance}
+            onChangeText={setDistance}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.buttonContainer} onPress={calculateMilage}>
+          <Text style={styles.buttonText}>Calculate</Text>
+        </TouchableOpacity>
+
+        {milage !== null && (
+          <Text style={styles.result}>
+            Mileage: {milage} km/l
+          </Text>
+        )}
+      </View>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+
+  container: {
+    flex: 1,
+    backgroundColor: "#b1ec6f",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  header: {
+    paddingTop: 65,
+    paddingBottom: 18,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#2a2a2a",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    gap: 10,
+    paddingLeft: 30,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  header2: {
+    paddingTop: 15,
+    paddingLeft: 20,
+    marginBottom: 10,
   },
+
+  headerIcon: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ffffffff",
+    borderRadius: 50,
+    padding: 10,
+  },
+
+  headerIconText: {
+    fontSize: 22,
+  },
+
+  headerText: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    gap: 5,
+  },
+
+  title2: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#000000ff",
+  },
+
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#000000ff",
+    letterSpacing: 0.5,
+  },
+
+  subTitle: {
+    fontSize: 14,
+    fontWeight: "normal",
+    color: "#000000ff",
+    letterSpacing: 0.5,
+  },
+
+  content: {
+    flex: 1,
+    alignItems: "center",
+    padding: 18,
+    paddingTop: 5,
+  },
+
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    marginTop: 10,
+    backgroundColor: "#ffffffff",
+    borderRadius: 25,
+    padding: 10,
+    paddingVertical: 25,
+  },
+
+  input: {
+    width: "90%",
+    height: 50,
+    borderColor: "#a79696ff",
+    borderWidth: 1,
+    fontSize: 16,
+    paddingHorizontal: 10,
+    borderRadius: 15,
+    color: "#000000ff",
+  },
+
+  input2: {
+    width: "90%",
+    height: 50,
+    borderColor: "#a79696ff",
+    borderWidth: 1,
+    fontSize: 16,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    color: "#fff",
+    marginBottom: 20,
+  },
+
+  inputLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#000000ff",
+    marginBottom: 10,
+    alignSelf: "flex-start",
+    marginLeft: "5%",
+  },
+
+  buttonContainer: {
+    width: "95%",
+    marginTop: 25,
+    paddingVertical: 18,
+    backgroundColor: "#174716",
+    borderRadius: 20,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  result: {
+    marginTop: 25,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+
 });
